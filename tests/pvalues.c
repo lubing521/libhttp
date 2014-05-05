a4e6fdc121f91650440d132192ffdc72f1c667f7
Nicolas Martyanoff <khaelin@gmail.com>
2014-05-05 10:57:42 +0200

    use libutest for the pvalue test suite

--- tests/pvalues.c
+++ tests/pvalues.c
@@ -19,16 +19,13 @@
 
 #include "tests.h"
 
-int
-main(int argc, char **argv) {
+TEST(base) {
     struct http_pvalue pvalue;
 
-#define HTTPT_BEGIN(str_)                                       \
-    do {                                                        \
-        if (http_pvalue_parse(&pvalue, str_, NULL) == -1) {     \
-            HTTPT_DIE("%s:%d: cannot parse pvalue: %s",         \
-                      __FILE__, __LINE__, http_get_error());    \
-        }                                                       \
+#define HTTPT_BEGIN(str_)                                            \
+    do {                                                             \
+        if (http_pvalue_parse(&pvalue, str_, NULL) == -1)            \
+            TEST_ABORT("cannot parse pvalue: %s", http_get_error()); \
     } while (0)
 
 #define HTTPT_END(str_)            \
@@ -36,23 +33,22 @@ main(int argc, char **argv) {
         http_pvalue_free(&pvalue); \
     } while (0)
 
-#define HTTPT_PVALUE_PARAMETER_IS(name_, value_)                         \
-    do {                                                                 \
-        HTTPT_IS_TRUE(http_pvalue_has_parameter(&pvalue, name_));        \
-        HTTPT_IS_EQUAL_STRING(http_pvalue_get_parameter(&pvalue, name_), \
-                              value_);                                   \
+#define HTTPT_PVALUE_PARAMETER_IS(name_, value_)                           \
+    do {                                                                   \
+        TEST_TRUE(http_pvalue_has_parameter(&pvalue, name_));              \
+        TEST_STRING_EQ(http_pvalue_get_parameter(&pvalue, name_), value_); \
     } while (0)
 
     HTTPT_BEGIN("a");
-    HTTPT_IS_EQUAL_STRING(pvalue.value, "a");
+    TEST_STRING_EQ(pvalue.value, "a");
     HTTPT_END();
 
     HTTPT_BEGIN("foo");
-    HTTPT_IS_EQUAL_STRING(pvalue.value, "foo");
+    TEST_STRING_EQ(pvalue.value, "foo");
     HTTPT_END();
 
     HTTPT_BEGIN("foo;a=1; b=2  ;c=3   ;  d=4");
-    HTTPT_IS_EQUAL_STRING(pvalue.value, "foo");
+    TEST_STRING_EQ(pvalue.value, "foo");
     HTTPT_PVALUE_PARAMETER_IS("a", "1");
     HTTPT_PVALUE_PARAMETER_IS("b", "2");
     HTTPT_PVALUE_PARAMETER_IS("c", "3");
@@ -60,18 +56,20 @@ main(int argc, char **argv) {
     HTTPT_END();
 
     HTTPT_BEGIN("foo; a=foo; b=\"foo\"; c=\"\\\"foo\\\"\"");
-    HTTPT_IS_EQUAL_STRING(pvalue.value, "foo");
+    TEST_STRING_EQ(pvalue.value, "foo");
     HTTPT_PVALUE_PARAMETER_IS("a", "foo");
     HTTPT_PVALUE_PARAMETER_IS("b", "foo");
     HTTPT_PVALUE_PARAMETER_IS("c", "\"foo\"");
     HTTPT_END();
+}
 
-#define HTTPT_INVALID_PVALUE(str_)                         \
-    do {                                                   \
-        if (http_pvalue_parse(&pvalue, str_, NULL) == 0) { \
-            HTTPT_DIE("%s:%d: parsed invalid pvalue",      \
-                      __FILE__, __LINE__);                 \
-        }                                                  \
+TEST(invalid) {
+#define HTTPT_INVALID_PVALUE(str_)                       \
+    do {                                                 \
+        struct http_pvalue pvalue;                       \
+                                                         \
+        if (http_pvalue_parse(&pvalue, str_, NULL) == 0) \
+            TEST_ABORT("parsed invalid pvalue");         \
     } while (0)
 
     /* Invalid value */
@@ -92,13 +90,20 @@ main(int argc, char **argv) {
     HTTPT_INVALID_PVALUE("foo; a=\"\\\"");
     HTTPT_INVALID_PVALUE("foo; a=\"\\x\"");
     HTTPT_INVALID_PVALUE("foo; a=\"foo\";");
-
-#undef HTTPT_INVALID_PVALUE
-
-#undef HTTPT_BEGIN
-#undef HTTPT_END
-#undef HTTPT_PVALUE_PARAMETER_IS
-
-    return 0;
 }
 
+
+int
+main(int argc, char **argv) {
+    struct test_suite *suite;
+
+    suite = test_suite_new("pvalues");
+    test_suite_initialize_from_args(suite, argc, argv);
+
+    test_suite_start(suite);
+
+    TEST_RUN(suite, base);
+    TEST_RUN(suite, invalid);
+
+    test_suite_print_results_and_exit(suite);
+}
