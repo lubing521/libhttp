fb5ac7b7e916a5b147c9bfaee3294b3486fd6107
Nicolas Martyanoff <khaelin@gmail.com>
2014-05-05 10:33:53 +0200

    use libutest for the uris test suite

--- tests/uris.c
+++ tests/uris.c
@@ -19,190 +19,186 @@
 
 #include "tests.h"
 
-int
-main(int argc, char **argv) {
+TEST(base) {
     struct http_uri *uri;
-    struct http_query_parameter *parameters;
-    size_t nb_parameters;
 
-    /* --------------------------------------------------------------------
-     *  URI
-     * -------------------------------------------------------------------- */
-#define HTTPT_BEGIN(str_)                                    \
-    do {                                                     \
-        uri = http_uri_new(str_);                            \
-        if (!uri) {                                          \
-            HTTPT_DIE("%s:%d: cannot parse uri: %s",         \
-                      __FILE__, __LINE__, http_get_error()); \
-        }                                                    \
+#define HTTPT_BEGIN(str_)                                         \
+    do {                                                          \
+        uri = http_uri_new(str_);                                 \
+        if (!uri)                                                 \
+            TEST_ABORT("cannot parse uri: %s", http_get_error()); \
     } while (0)
 
 #define HTTPT_END() \
     http_uri_delete(uri);
 
     HTTPT_BEGIN("/");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
+    TEST_STRING_EQ(uri->path, "/");
     HTTPT_END();
 
     HTTPT_BEGIN("/foo/bar");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/foo/bar");
+    TEST_STRING_EQ(uri->path, "/foo/bar");
     HTTPT_END();
 
     HTTPT_BEGIN("/foo/bar?a=1&b=2");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/foo/bar");
-    HTTPT_IS_TRUE(http_uri_has_query_parameter(uri, "a"));
-    HTTPT_IS_EQUAL_STRING(http_uri_query_parameter(uri, "a"), "1");
-    HTTPT_IS_TRUE(http_uri_has_query_parameter(uri, "b"));
-    HTTPT_IS_EQUAL_STRING(http_uri_query_parameter(uri, "b"), "2");
+    TEST_STRING_EQ(uri->path, "/foo/bar");
+    TEST_TRUE(http_uri_has_query_parameter(uri, "a"));
+    TEST_STRING_EQ(http_uri_query_parameter(uri, "a"), "1");
+    TEST_TRUE(http_uri_has_query_parameter(uri, "b"));
+    TEST_STRING_EQ(http_uri_query_parameter(uri, "b"), "2");
     HTTPT_END();
 
     HTTPT_BEGIN("http://127.0.0.1");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "127.0.0.1");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "127.0.0.1");
+    TEST_STRING_EQ(uri->path, "/");
     HTTPT_END();
 
     HTTPT_BEGIN("http://127.0.0.1:443");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "127.0.0.1");
-    HTTPT_IS_EQUAL_STRING(uri->port, "443");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "127.0.0.1");
+    TEST_STRING_EQ(uri->port, "443");
+    TEST_STRING_EQ(uri->path, "/");
     HTTPT_END();
 
     HTTPT_BEGIN("http://[::1]");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "::1");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "::1");
+    TEST_STRING_EQ(uri->path, "/");
     HTTPT_END();
 
     HTTPT_BEGIN("http://[::1]:443");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "::1");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
-    HTTPT_IS_EQUAL_STRING(uri->port, "443");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "::1");
+    TEST_STRING_EQ(uri->path, "/");
+    TEST_STRING_EQ(uri->port, "443");
     HTTPT_END();
 
     HTTPT_BEGIN("http://[fe80::1e6f:65ff:EA21:A373]:443");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "fe80::1e6f:65ff:EA21:A373");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
-    HTTPT_IS_EQUAL_STRING(uri->port, "443");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "fe80::1e6f:65ff:EA21:A373");
+    TEST_STRING_EQ(uri->path, "/");
+    TEST_STRING_EQ(uri->port, "443");
     HTTPT_END();
 
     HTTPT_BEGIN("http://example.com");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "example.com");
+    TEST_STRING_EQ(uri->path, "/");
     HTTPT_END();
 
     HTTPT_BEGIN("http://example.com:8080");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
-    HTTPT_IS_EQUAL_STRING(uri->port, "8080");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "example.com");
+    TEST_STRING_EQ(uri->port, "8080");
+    TEST_STRING_EQ(uri->path, "/");
     HTTPT_END();
 
     HTTPT_BEGIN("http://example.com/");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "example.com");
+    TEST_STRING_EQ(uri->path, "/");
     HTTPT_END();
 
     HTTPT_BEGIN("http://example.com/#foo");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
-    HTTPT_IS_EQUAL_STRING(uri->fragment, "foo");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "example.com");
+    TEST_STRING_EQ(uri->path, "/");
+    TEST_STRING_EQ(uri->fragment, "foo");
     HTTPT_END();
 
     HTTPT_BEGIN("http://example.com#foo");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
-    HTTPT_IS_EQUAL_STRING(uri->fragment, "foo");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "example.com");
+    TEST_STRING_EQ(uri->path, "/");
+    TEST_STRING_EQ(uri->fragment, "foo");
     HTTPT_END();
 
     HTTPT_BEGIN("http://example.com:8080/");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
-    HTTPT_IS_EQUAL_STRING(uri->port, "8080");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "example.com");
+    TEST_STRING_EQ(uri->port, "8080");
+    TEST_STRING_EQ(uri->path, "/");
     HTTPT_END();
 
     HTTPT_BEGIN("http://foo@example.com");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->user, "foo");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->user, "foo");
+    TEST_STRING_EQ(uri->host, "example.com");
     HTTPT_END();
 
     HTTPT_BEGIN("http://foo:bar@example.com");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->user, "foo");
-    HTTPT_IS_EQUAL_STRING(uri->password, "bar");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->user, "foo");
+    TEST_STRING_EQ(uri->password, "bar");
+    TEST_STRING_EQ(uri->host, "example.com");
     HTTPT_END();
 
     HTTPT_BEGIN("http://example.com/a/b/c");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/a/b/c");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "example.com");
+    TEST_STRING_EQ(uri->path, "/a/b/c");
     HTTPT_END();
 
     HTTPT_BEGIN("http://example.com/?a=1&b=2");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
-    HTTPT_IS_TRUE(http_uri_has_query_parameter(uri, "a"));
-    HTTPT_IS_EQUAL_STRING(http_uri_query_parameter(uri, "a"), "1");
-    HTTPT_IS_TRUE(http_uri_has_query_parameter(uri, "b"));
-    HTTPT_IS_EQUAL_STRING(http_uri_query_parameter(uri, "b"), "2");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "example.com");
+    TEST_STRING_EQ(uri->path, "/");
+    TEST_TRUE(http_uri_has_query_parameter(uri, "a"));
+    TEST_STRING_EQ(http_uri_query_parameter(uri, "a"), "1");
+    TEST_TRUE(http_uri_has_query_parameter(uri, "b"));
+    TEST_STRING_EQ(http_uri_query_parameter(uri, "b"), "2");
     HTTPT_END();
 
     HTTPT_BEGIN("http://example.com/a/b/c?a=1&b=2");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/a/b/c");
-    HTTPT_IS_TRUE(http_uri_has_query_parameter(uri, "a"));
-    HTTPT_IS_EQUAL_STRING(http_uri_query_parameter(uri, "a"), "1");
-    HTTPT_IS_TRUE(http_uri_has_query_parameter(uri, "b"));
-    HTTPT_IS_EQUAL_STRING(http_uri_query_parameter(uri, "b"), "2");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "example.com");
+    TEST_STRING_EQ(uri->path, "/a/b/c");
+    TEST_TRUE(http_uri_has_query_parameter(uri, "a"));
+    TEST_STRING_EQ(http_uri_query_parameter(uri, "a"), "1");
+    TEST_TRUE(http_uri_has_query_parameter(uri, "b"));
+    TEST_STRING_EQ(http_uri_query_parameter(uri, "b"), "2");
     HTTPT_END();
 
     HTTPT_BEGIN("http://example.com/a/b/c?a=1&b=2#foo");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/a/b/c");
-    HTTPT_IS_TRUE(http_uri_has_query_parameter(uri, "a"));
-    HTTPT_IS_EQUAL_STRING(http_uri_query_parameter(uri, "a"), "1");
-    HTTPT_IS_TRUE(http_uri_has_query_parameter(uri, "b"));
-    HTTPT_IS_EQUAL_STRING(http_uri_query_parameter(uri, "b"), "2");
-    HTTPT_IS_EQUAL_STRING(uri->fragment, "foo");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "example.com");
+    TEST_STRING_EQ(uri->path, "/a/b/c");
+    TEST_TRUE(http_uri_has_query_parameter(uri, "a"));
+    TEST_STRING_EQ(http_uri_query_parameter(uri, "a"), "1");
+    TEST_TRUE(http_uri_has_query_parameter(uri, "b"));
+    TEST_STRING_EQ(http_uri_query_parameter(uri, "b"), "2");
+    TEST_STRING_EQ(uri->fragment, "foo");
     HTTPT_END();
 
     HTTPT_BEGIN("http://foo:bar@example.com:8080/path?a=1&b=2");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->user, "foo");
-    HTTPT_IS_EQUAL_STRING(uri->password, "bar");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
-    HTTPT_IS_EQUAL_STRING(uri->port, "8080");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/path");
-    HTTPT_IS_TRUE(http_uri_has_query_parameter(uri, "a"));
-    HTTPT_IS_EQUAL_STRING(http_uri_query_parameter(uri, "a"), "1");
-    HTTPT_IS_TRUE(http_uri_has_query_parameter(uri, "b"));
-    HTTPT_IS_EQUAL_STRING(http_uri_query_parameter(uri, "b"), "2");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->user, "foo");
+    TEST_STRING_EQ(uri->password, "bar");
+    TEST_STRING_EQ(uri->host, "example.com");
+    TEST_STRING_EQ(uri->port, "8080");
+    TEST_STRING_EQ(uri->path, "/path");
+    TEST_TRUE(http_uri_has_query_parameter(uri, "a"));
+    TEST_STRING_EQ(http_uri_query_parameter(uri, "a"), "1");
+    TEST_TRUE(http_uri_has_query_parameter(uri, "b"));
+    TEST_STRING_EQ(http_uri_query_parameter(uri, "b"), "2");
     HTTPT_END();
 
     HTTPT_BEGIN("%68%74%74%70://%65%78%61%6d%70%6c%65.%63%6f%6D");
-    HTTPT_IS_EQUAL_STRING(uri->scheme, "http");
-    HTTPT_IS_EQUAL_STRING(uri->host, "example.com");
-    HTTPT_IS_EQUAL_STRING(uri->path, "/");
+    TEST_STRING_EQ(uri->scheme, "http");
+    TEST_STRING_EQ(uri->host, "example.com");
+    TEST_STRING_EQ(uri->path, "/");
     HTTPT_END();
+}
 
+TEST(invalid) {
 #define HTTPT_INVALID_URI(str_)                                         \
     do {                                                                \
+        struct http_uri *uri;                                           \
+                                                                        \
         uri = http_uri_new(str_);                                       \
         if (uri)                                                        \
-            HTTPT_DIE("%s:%d: parsed invalid uri", __FILE__, __LINE__); \
+            TEST_ABORT("parsed invalid uri");                           \
     } while (0)
 
     /* Invalid string */
@@ -240,93 +236,93 @@ main(int argc, char **argv) {
     HTTPT_INVALID_URI("http://example.co%");
     HTTPT_INVALID_URI("http://example.co%6");
     HTTPT_INVALID_URI("http://example.co%g");
+}
 
-#undef HTTPT_INVALID_URI
+TEST(query) {
+    struct http_query_parameter *parameters;
+    size_t nb_parameters;
 
-#undef HTTPT_BEGIN
-#undef HTTPT_END
-
-    /* --------------------------------------------------------------------
-     *  Query string
-     * -------------------------------------------------------------------- */
-#define HTTPT_BEGIN(str_)                                                     \
+#define HTTPT_BEGIN_QUERY(str_)                                               \
     do {                                                                      \
         if (http_query_parameters_parse(str_,                                 \
                                         &parameters, &nb_parameters) == -1) { \
-            HTTPT_DIE("%s:%d: cannot parse query: %s",                        \
-                      __FILE__, __LINE__, http_get_error());                  \
+            TEST_ABORT("cannot parse query: %s", http_get_error());           \
         }                                                                     \
     } while (0)
 
-#define HTTPT_END()                                    \
+#define HTTPT_END_QUERY()                              \
     do {                                               \
         for (size_t i = 0; i < nb_parameters; i++)     \
             http_query_parameter_free(parameters + i); \
         http_free(parameters);                         \
     } while (0)
 
-    HTTPT_BEGIN("");
-    HTTPT_IS_EQUAL_UINT(nb_parameters, 0);
-    HTTPT_END();
+    HTTPT_BEGIN_QUERY("");
+    TEST_UINT_EQ(nb_parameters, 0);
+    HTTPT_END_QUERY();
 
-    HTTPT_BEGIN("a");
-    HTTPT_IS_EQUAL_UINT(nb_parameters, 1);
-    HTTPT_IS_EQUAL_STRING(parameters[0].name, "a");
-    HTTPT_IS_EQUAL_PTR(parameters[0].value, NULL);
-    HTTPT_END();
+    HTTPT_BEGIN_QUERY("a");
+    TEST_UINT_EQ(nb_parameters, 1);
+    TEST_STRING_EQ(parameters[0].name, "a");
+    TEST_PTR_NULL(parameters[0].value);
+    HTTPT_END_QUERY();
 
-    HTTPT_BEGIN("foo");
-    HTTPT_IS_EQUAL_UINT(nb_parameters, 1);
-    HTTPT_IS_EQUAL_STRING(parameters[0].name, "foo");
-    HTTPT_IS_EQUAL_PTR(parameters[0].value, NULL);
-    HTTPT_END();
+    HTTPT_BEGIN_QUERY("foo");
+    TEST_UINT_EQ(nb_parameters, 1);
+    TEST_STRING_EQ(parameters[0].name, "foo");
+    TEST_PTR_NULL(parameters[0].value);
+    HTTPT_END_QUERY();
 
-    HTTPT_BEGIN("foo=bar");
-    HTTPT_IS_EQUAL_UINT(nb_parameters, 1);
-    HTTPT_IS_EQUAL_STRING(parameters[0].name, "foo");
-    HTTPT_IS_EQUAL_STRING(parameters[0].value, "bar");
-    HTTPT_END();
+    HTTPT_BEGIN_QUERY("foo=bar");
+    TEST_UINT_EQ(nb_parameters, 1);
+    TEST_STRING_EQ(parameters[0].name, "foo");
+    TEST_STRING_EQ(parameters[0].value, "bar");
+    HTTPT_END_QUERY();
 
-    HTTPT_BEGIN("a=1&b=2");
-    HTTPT_IS_EQUAL_UINT(nb_parameters, 2);
-    HTTPT_IS_EQUAL_STRING(parameters[0].name, "a");
-    HTTPT_IS_EQUAL_STRING(parameters[0].value, "1");
-    HTTPT_IS_EQUAL_STRING(parameters[1].name, "b");
-    HTTPT_IS_EQUAL_STRING(parameters[1].value, "2");
-    HTTPT_END();
+    HTTPT_BEGIN_QUERY("a=1&b=2");
+    TEST_UINT_EQ(nb_parameters, 2);
+    TEST_STRING_EQ(parameters[0].name, "a");
+    TEST_STRING_EQ(parameters[0].value, "1");
+    TEST_STRING_EQ(parameters[1].name, "b");
+    TEST_STRING_EQ(parameters[1].value, "2");
+    HTTPT_END_QUERY();
 
-    HTTPT_BEGIN("a&b=2;c&d=3");
-    HTTPT_IS_EQUAL_UINT(nb_parameters, 4);
-    HTTPT_IS_EQUAL_STRING(parameters[0].name, "a");
-    HTTPT_IS_EQUAL_PTR(parameters[0].value, NULL);
-    HTTPT_IS_EQUAL_STRING(parameters[1].name, "b");
-    HTTPT_IS_EQUAL_STRING(parameters[1].value, "2");
-    HTTPT_IS_EQUAL_STRING(parameters[2].name, "c");
-    HTTPT_IS_EQUAL_PTR(parameters[2].value, NULL);
-    HTTPT_IS_EQUAL_STRING(parameters[3].name, "d");
-    HTTPT_IS_EQUAL_STRING(parameters[3].value, "3");
-    HTTPT_END();
+    HTTPT_BEGIN_QUERY("a&b=2;c&d=3");
+    TEST_UINT_EQ(nb_parameters, 4);
+    TEST_STRING_EQ(parameters[0].name, "a");
+    TEST_PTR_NULL(parameters[0].value);
+    TEST_STRING_EQ(parameters[1].name, "b");
+    TEST_STRING_EQ(parameters[1].value, "2");
+    TEST_STRING_EQ(parameters[2].name, "c");
+    TEST_PTR_NULL(parameters[2].value);
+    TEST_STRING_EQ(parameters[3].name, "d");
+    TEST_STRING_EQ(parameters[3].value, "3");
+    HTTPT_END_QUERY();
 
-    HTTPT_BEGIN("a+b+=%20%c3%a9tat");
-    HTTPT_IS_EQUAL_UINT(nb_parameters, 1);
-    HTTPT_IS_EQUAL_STRING(parameters[0].name, "a b ");
-    HTTPT_IS_EQUAL_STRING(parameters[0].value, " état");
-    HTTPT_END();
+    HTTPT_BEGIN_QUERY("a+b+=%20%c3%a9tat");
+    TEST_UINT_EQ(nb_parameters, 1);
+    TEST_STRING_EQ(parameters[0].name, "a b ");
+    TEST_STRING_EQ(parameters[0].value, " état");
+    HTTPT_END_QUERY();
 
-    HTTPT_BEGIN("%3d=%20%26&%3b=++");
-    HTTPT_IS_EQUAL_UINT(nb_parameters, 2);
-    HTTPT_IS_EQUAL_STRING(parameters[0].name, "=");
-    HTTPT_IS_EQUAL_STRING(parameters[0].value, " &");
-    HTTPT_IS_EQUAL_STRING(parameters[1].name, ";");
-    HTTPT_IS_EQUAL_STRING(parameters[1].value, "  ");
-    HTTPT_END();
+    HTTPT_BEGIN_QUERY("%3d=%20%26&%3b=++");
+    TEST_UINT_EQ(nb_parameters, 2);
+    TEST_STRING_EQ(parameters[0].name, "=");
+    TEST_STRING_EQ(parameters[0].value, " &");
+    TEST_STRING_EQ(parameters[1].name, ";");
+    TEST_STRING_EQ(parameters[1].value, "  ");
+    HTTPT_END_QUERY();
+}
 
+TEST(invalid_query) {
 #define HTTPT_INVALID_QUERY(str_)                                             \
     do {                                                                      \
+        struct http_query_parameter *parameters;                              \
+        size_t nb_parameters;                                                 \
+                                                                              \
         if (http_query_parameters_parse(str_,                                 \
                                         &parameters, &nb_parameters) == 0) {  \
-            HTTPT_DIE("%s:%d: parsed invalid query",                          \
-                      __FILE__, __LINE__);                                    \
+            TEST_ABORT("parsed invalid query");                               \
         }                                                                     \
     } while (0)
 
@@ -334,11 +330,21 @@ main(int argc, char **argv) {
     HTTPT_INVALID_QUERY("a=1&");
     HTTPT_INVALID_QUERY("a=");
     HTTPT_INVALID_QUERY("=1");
+}
 
-#undef HTTPT_INVALID_QUERY
+int
+main(int argc, char **argv) {
+    struct test_suite *suite;
 
-#undef HTTPT_BEGIN
-#undef HTTPT_END
+    suite = test_suite_new("uris");
+    test_suite_initialize_from_args(suite, argc, argv);
 
-    return 0;
+    test_suite_start(suite);
+
+    TEST_RUN(suite, base);
+    TEST_RUN(suite, invalid);
+    TEST_RUN(suite, query);
+    TEST_RUN(suite, invalid_query);
+
+    test_suite_print_results_and_exit(suite);
 }
